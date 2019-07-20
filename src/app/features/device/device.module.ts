f2884c8af88e7d00175914bc3685e6ab6df8156a
itea-dev <itea.dev@outlook.com>
2019-07-20 19:33:09 +0200

    Add device module

---
+++ src/app/features/device/device.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DeviceRoutingModule } from './device-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DeviceRoutingModule
+  ]
+})
+export class DeviceModule { }
