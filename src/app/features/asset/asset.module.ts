47dd2895c8e36bbc6068c8578eb79d646ef33608
itea-dev <itea.dev@outlook.com>
2019-07-20 19:32:20 +0200

    Add asset module

---
+++ src/app/features/asset/asset.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AssetRoutingModule } from './asset-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AssetRoutingModule
+  ]
+})
+export class AssetModule { }
