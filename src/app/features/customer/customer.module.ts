09e43657a61ff04a2d894227821f2cfaff2a4427
itea-dev <itea.dev@outlook.com>
2019-07-20 19:32:45 +0200

    Add customer module

---
+++ src/app/features/customer/customer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomerRoutingModule } from './customer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomerRoutingModule
+  ]
+})
+export class CustomerModule { }
