69445aa4c6e207ad1cfe1540bad9d568997db645
itea-dev <itea.dev@outlook.com>
2019-07-21 16:42:07 +0200

    Remove other modules from angular material module

--- src/app/shared/angular-material.module.ts
+++ src/app/shared/angular-material.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { CdkTableModule } from '@angular/cdk/table';
-import { CdkTreeModule } from '@angular/cdk/tree';
 
 import {
   MatAutocompleteModule,
@@ -41,7 +37,7 @@ import {
   MatTabsModule,
   MatToolbarModule,
   MatTooltipModule,
-  MatTreeModule,
+  MatTreeModule
 } from '@angular/material';
 
 
@@ -51,9 +47,6 @@ import {
     CommonModule
   ],
   exports: [
-    CdkTableModule,
-    CdkTreeModule,
-    DragDropModule,
     MatAutocompleteModule,
     MatBadgeModule,
     MatBottomSheetModule,
@@ -89,7 +82,8 @@ import {
     MatToolbarModule,
     MatTooltipModule,
     MatTreeModule,
-    ScrollingModule
+    ScrollingModule,
+    MatSidenavModule
   ]
 })
 export class AngularMaterialModule { }
