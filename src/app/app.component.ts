d4543f49cd083acc0c6de5a940a1922a93595378
itea-dev <itea.dev@outlook.com>
2019-07-28 23:39:07 +0200

    Hide header & sidenav if user not logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { AuthService } from './core/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  isAuthenticated$: Observable<boolean>;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -17,7 +20,12 @@ export class AppComponent {
     );
 
   constructor(
+    private authService: AuthService,
     private breakpointObserver: BreakpointObserver
   ) { }
+
+  async ngOnInit() {
+    this.isAuthenticated$ = this.authService.getAuthenticationState();
+  }
 
 }
