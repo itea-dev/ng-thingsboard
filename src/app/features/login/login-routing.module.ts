8c5bb93b498e32da4f2a76f68723918b5e7cabf6
itea-dev <itea.dev@outlook.com>
2019-08-04 14:04:04 +0200

    Rename auth module to login module

--- src/app/features/login/login-routing.module.ts
+++ src/app/features/login/login-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-
+import { LoginComponent } from './login.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
 ];
 
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class LoginRoutingModule { }
--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
@@ -10,7 +9,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule,
+    LoginRoutingModule,
     SharedModule
   ]
 })
