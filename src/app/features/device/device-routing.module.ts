7d701af01b29c1e7f8fddd4cdd1730350e4b69bb
itea-dev <itea.dev@outlook.com>
2019-08-08 00:40:08 +0200

    Add devices component

--- src/app/features/device/device-routing.module.ts
+++ src/app/features/device/device-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DevicesComponent } from './devices.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DevicesComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/device/device.module.ts
+++ src/app/features/device/device.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DeviceRoutingModule } from './device-routing.module';
+import { DevicesComponent } from './devices.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DevicesComponent],
   imports: [
     CommonModule,
     DeviceRoutingModule
