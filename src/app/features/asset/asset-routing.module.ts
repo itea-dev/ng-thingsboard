cccafbeb39a35daaf10a6d009be2d0c311723808
itea-dev <itea.dev@outlook.com>
2019-08-08 00:40:58 +0200

    Update asset routing module
    Added default route

--- src/app/features/asset/asset-routing.module.ts
+++ src/app/features/asset/asset-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AssetsComponent } from './assets.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AssetsComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
