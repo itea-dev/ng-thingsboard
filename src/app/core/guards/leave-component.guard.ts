20b9feb27ff698c45c2a3e4b0480ae3b8f5dfa29
itea.dev <itea.dev@outlook.com>
2020-03-04 22:33:20 +0100

    Add leave component guard

---
+++ src/app/core/guards/leave-component.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaveComponentGuard implements CanDeactivate<unknown> {
+
+  canDeactivate(
+    component: any,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return component.canDeactivate();
+  }
+
+}
